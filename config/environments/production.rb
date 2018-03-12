4e47087efbd4dfa4db3b814659846f89877a218c
temy13 <temy.poporofier00@gmail.com>
2018-03-12 23:54:02 +0900

    reverse

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -108,6 +108,6 @@ Rails.application.configure do
   config.serve_static_assets = true
   config.public_file_server.enabled = true
 
-  GA.tracker = ENV["GA_TRACKING_CODE"]
+  GA.tracker = ENV["GA-TRACKING-CODE"]
 
 end

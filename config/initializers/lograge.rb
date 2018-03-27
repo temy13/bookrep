8a7e3bb5f56533ca0bbc8096866a2baa87e1a049
temy13 <temy.poporofier00@gmail.com>
2018-03-27 10:45:34 +0900

    bug fix

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -5,18 +5,23 @@ Rails.application.configure do
   config.lograge.formatter = Lograge::Formatters::Json.new
   config.lograge.custom_options = lambda do |event|
     exceptions = %w(controller action format authenticity_token)
-    {
+    data = {
+      level: 'info',
       time: event.time,
-      ua: event.payload[:ua],
+      user_agent: event.payload[:ua],
       params: event.payload[:params].except(*exceptions),
       login_id: event.payload[:login_id],
       ip: event.payload[:ip],
       remote_ip: event.payload[:remote_ip],
-      referer: event.payload[:referer],
-      exception: event.payload[:exception],
-      exception_object: event.payload[:exception_object],
-      exception_backtrace: event.payload[:exception_object].present? ? "" : event.payload[:exception_object].backtrace[0..6]
+      referer: event.payload[:referer]
     }
+    if event.payload[:exception]
+      data[:level] = 'fatal'
+      data[:exception] = event.payload[:exception]
+      data[:exception_object] = event.payload[:exception_object],
+      data[:exception_backtrace] = event.payload[:exception_object].backtrace[0..6]
+    end
+    data
   end
 
   config.lograge.keep_original_rails_log = true

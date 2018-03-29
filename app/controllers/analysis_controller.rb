94e656aa782fdebdfe2682ed5a27c671dda8a9a1
temy13 <temy.poporofier00@gmail.com>
2018-03-29 11:41:06 +0900

    a little fix

--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -1,6 +1,5 @@
-class AnalysisController < ApplicationController
+class AnalysisController < AdminBaseController
 
-  before_action :check_admin
 
   def index
     gon.mail_user_size = User.where(provider: nil, states_cd: User.states[:normal]).size
@@ -31,16 +30,5 @@ class AnalysisController < ApplicationController
 
   end
 
-  private
-  def check_admin
-    raise ActionController::RoutingError.new('Not Found') unless current_user.present? && current_user.admin?
-    basic
-  end
-
-  def basic
-      authenticate_or_request_with_http_basic do |user, pass|
-        user == 'user' && pass == 'admin'
-      end
-  end
 
 end

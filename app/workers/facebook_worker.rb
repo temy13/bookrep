3ccf19905f077c1d2a5819de78a3115c45b6e296
temy13 <temy.poporofier00@gmail.com>
2018-04-20 07:53:40 +0900

    fw

--- app/workers/facebook_worker.rb
+++ app/workers/facebook_worker.rb
@@ -4,15 +4,18 @@ class FacebookWorker include Sidekiq::Worker
   def perform(q_id)
 
     question = Question.find(q_id)
-    sleep 5
+    5.times do
+      break unless question.image.blank?
+      sleep 10
+    end
     return if question.image.blank?
     @api = Koala::Facebook::API.new(ENV["FACEBOOK_PAGE_TOKEN"])
     @api.put_wall_post(question.content, {
-      "name" => "投稿ページへのリンクです",
+      # "name" => "投稿ページへのリンクです",
       "link" => question.page_url,
-      "caption" => "新しい質問が投稿されました！",
-      "description" => question.content,
-      "picture" => ENV["SERVICE_HOST"] + question.image.sns.url
+      # "caption" => "新しい質問が投稿されました！",
+      # "description" => question.content,
+      # "picture" => ENV["SERVICE_HOST"] + question.image.sns.url
     })
 
   end
